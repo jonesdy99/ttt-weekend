0a9b988d6c957d5600899db151a7f141077f861f
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-01 15:07:57 -0400

    check that .squares returns all the items in the div

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ let board, turn, winner
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.squares')
 const messageEl = document.querySelector('#message')
+console.log(squareEls)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
