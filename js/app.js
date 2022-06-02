26d298641e3dc00279c7ac32cf7f7119dbfe6f14
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-02 19:33:40 -0400

    play around with solutions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
 const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,5,8],[1,4,7],[0,4,8],[2,4,6]]
 
+
 /*---------------------------- Variables (state) ----------------------------*/
 // let board, turn, winner
   let board = [null,null,null,null,null,null,null,null,null]
@@ -19,16 +20,12 @@ squareEls.forEach(function(squares){
   squares.addEventListener('click',handleClick)
 })
 
-
 /*-------------------------------- Functions --------------------------------*/
 
 init()
 
 
 function init() {
-  // board = [null,null,null,null,null,null,null,null,null]
-  // turn = 1
-  // winner = null
   render()
 }
 
@@ -42,13 +39,15 @@ function render(){
       squareEls[index].textContent = null
     }
   });
-  // renderMessage()
+  
 
   if(winner === null){
-    messageEl.textContent = `Keep going! It's ${turn} turn!`
-  } else if(winner === 'T'){
+    messageEl.textContent = `Keep going! Player ${turn === 1 ? "X" : "0"}'s turn!`
+  } else if(winner = 'T'){
     messageEl.textContent = `Oh! It's a tie! Go Again!`
-  } else messageEl.textContent = `Player ${turn === 1 ? "0" : 'X'} won!`
+  } else if (winner = 1 || -1){
+    messageEl.textContent = `Player ${turn === 1 ? "0" : "X"} won!`
+  } 
 }
 
 function handleClick(evt){
@@ -67,18 +66,18 @@ function handleClick(evt){
 }
 
 function getWinner() {
-    
-  for (let i=0; i < winningCombos.length; i++) {
+    for (let i=0; i < winningCombos.length; i++) {
       let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]
       if (sum === 3) {
-          return 1
+          winner = 1
       } else  if (sum === -3) {
-          return -1
+          winner = -1
       } else if (board.includes(null) === false) {
-          return 'T'
+          winner = 'T'
+      }else {
+          winner = null
       }
   }
-  return null
 }
 
 
